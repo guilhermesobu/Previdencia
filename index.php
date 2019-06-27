689f28b01c40b3bdf449c123c747ba4399219dca
guilhermesobu <guilherme.sobu@outlook.com>
2019-06-27 19:23:11 -0300

    Colocando o repositorio no GIT

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
+        <title>Previcência</title>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+        
+        
+        <script src="assets/js/jquery-3.4.1.js"></script>
+        <script src="assets/js/popper.min.js"></script>
+        <script src="assets/js/bootstrap.js"></script> 
+    </body>
+</html>
